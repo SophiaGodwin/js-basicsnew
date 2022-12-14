e8be825e9df0b180b664fb5bfb502571dbc170b2
SophiaG <sophiagodwin@gmail.com>
2022-12-14 09:58:00 -0500

    js basicsnew

---
+++ js-basics02.js
@@ -0,0 +1,44 @@
+var name = 'Adam' //string
+name = 'Eva'
+
+var age =10 //number
+
+var email = 'shn@test.com'
+var address = '123 Main St , San Jose CA 95132'
+var a = 10
+var sum = 10+20 // 30
+var x = a+10 //'20'
+
+var foo = 'hello' + 'world!' // 'hello world!'
+
+var isHuman = true //boolean
+isHuman = false
+
+var isVaccinated = false
+
+var ageofPerson = 40
+
+var x1 = 1
+var y1 = 2
+var z1 = x1+y1; // 3 
+
+console.log('hello world')
+console.log(x)
+
+console.log(10+40)
+
+console.log('a'+45)
+
+var xx = 100
+var yy = 200
+xx = xx/2 //40
+//console.log(xx/10);
+console.log( xx==100)
+
+var x = 10;
+console.log(x+30)
+
+
+
+
+
